a5ecdfe387f852f4a44ab2ebf9947ce90c8eb985
Soumya <Soumya@DESKTOP-8A5TPBL>
2025-03-14 16:56:24 +0530

    POM for homepage

---
+++ src/test/java/page/HomePage.java
@@ -0,0 +1,38 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+
+public class HomePage 
+
+{
+@FindBy(xpath="//h3[starts-with(text(), 'Welcome to OSPOS')]")
+private WebElement welcomeMessage;
+
+public HomePage(WebDriver driver)
+{
+	PageFactory.initElements(driver, this);
+}
+
+public boolean verifyHomePageIsDisplayed(WebDriverWait wait)
+{
+	try
+	{
+		wait.until(ExpectedConditions.visibilityOf(welcomeMessage));
+		Reporter.log("home page is displayed, true");
+		return true;
+	}
+    catch (Exception e)
+	{
+    	Reporter.log("home page is  not displayed, true");
+    	return false;
+	}
+	
+}
+}
